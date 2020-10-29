e2f7d586a41c5bd0cd24e7fcf7cbb92607d2bc4e
Franz Delomen <franz.delomen@rsa.com.ph>
2020-10-29 14:01:33 +0800

    fix software for incharge

--- resources/views/signagejo.blade.php
+++ resources/views/signagejo.blade.php
@@ -61,12 +61,14 @@
                                         </div>
                                         <div class="row">
                                             <div class="col-md-6">
+                                                @if($jo->SOFTWAREINCHARGE <> 'N/A')
                                                 <div class="card border-primary" style="font-size:15px;margin-top:10px;">
                                                     <div class="card-header bg-primary text-white">SOFTWARE IN CHARGE</div>
                                                     <div class="card-body text-primary">
                                                         <p class="card-text">{{$jo->SOFTWAREINCHARGE}}</p>
                                                     </div>
                                                 </div>
+                                                @endif
                                             </div>
                                             <div class="col-md-6">
                                                 <div class="card border-info" style="font-size:15px;margin-top:10px;">
