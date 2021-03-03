598bffaace35f47c51372b795b525ddbd87c5159
Franz Delomen <franz.delomen@rsa.com.ph>
2021-03-03 17:49:16 +0800

    return to original

--- resources/views/signage.blade.php
+++ resources/views/signage.blade.php
@@ -20,7 +20,7 @@
                         </div>
                     @else
                         <div class="carousel-item">
-                            <video src="/{{$sign->source_url}}"></video>
+                            <video src="/{{$sign->source_url}}" muted autoplay></video>
                         </div>
                     @endif
                 @endforeach
