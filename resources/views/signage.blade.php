6659b22884ec46c39f9079a89041b09491015ea0
Franz Delomen <franz.delomen@rsa.com.ph>
2021-03-03 17:40:09 +0800

    unmutefix

--- resources/views/signage.blade.php
+++ resources/views/signage.blade.php
@@ -69,6 +69,7 @@
                 }
 
                 $('#unMute').on("click",function(){
+                    console.log("muted");
                     $('video').muted = false;
                 });
 
