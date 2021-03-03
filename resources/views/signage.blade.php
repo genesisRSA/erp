308d2178074b32853de3a2d0e02883bed1882fcf
Franz Delomen <franz.delomen@rsa.com.ph>
2021-03-03 17:53:56 +0800

    test iframe

--- resources/views/signage.blade.php
+++ resources/views/signage.blade.php
@@ -21,6 +21,7 @@
                     @else
                         <div class="carousel-item">
                             <video src="/{{$sign->source_url}}" muted autoplay></video>
+                            <iframe src="/{{$sign->source_url}}" allow="autoplay" style="display:none"></iframe>
                         </div>
                     @endif
                 @endforeach
@@ -75,6 +76,7 @@
                     if(currentIndex == totalItems){
                         location.reload();
                     }else{
+                        $('iframe').src = $('iframe').src
                         $("#carousel").carousel('cycle');
                     }
                 });
